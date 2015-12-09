a0f4f6e9d22cd2d104281c92d0af06e91eb280e0
Diego Lirio <diegolirio.dl@gmail.com>
2015-12-09 17:09:07 -0200

    ...

--- deploy/1.0.4/script.sql
+++ deploy/1.0.4/script.sql
@@ -9,3 +9,12 @@ Update Jogo j
 Update Jogo j
   set j.vencedor = 'E'
   where j.resultadoA = j.resultadoB;  
+  
+
+update Evento e 
+  set e.imgName = 'gol_16x16.png' 
+  where id = 1;
+  
+update Evento e set e.imgName = 'cartao-amarelo_16.png' where id = 2;
+
+update Evento e set e.imgName = 'cartao-vermelho_16.png' where id = 3;
